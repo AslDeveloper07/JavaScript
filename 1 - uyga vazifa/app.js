18b62d9a64f59a7dd0db746aaf67a209b6835a4c
AslDeveloper07 <aslwexking777@gmail.com>
2025-04-27 15:11:32 +0500

    4 problem

--- 1 - uyga vazifa/app.js
+++ 1 - uyga vazifa/app.js
@@ -288,6 +288,16 @@ function isPrime(n) {
 }
 
 //9 - MASALA
+function maxInArray(arr) {
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
 //10 - MASALA
 //11 - MASALA
 //12 - MASALA
