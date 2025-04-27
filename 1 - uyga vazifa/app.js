75a0ac747842438aade216ac4f9a29883b79376c
AslDeveloper07 <aslwexking777@gmail.com>
2025-04-27 15:15:07 +0500

    add

--- 1 - uyga vazifa/app.js
+++ 1 - uyga vazifa/app.js
@@ -390,5 +390,13 @@ function countVowels(str) {
 }
 
 //18 - MASALA
+function reverseArray(arr) {
+  let reversed = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    reversed.push(arr[i]);
+  }
+  return reversed;
+}
+
 
 
