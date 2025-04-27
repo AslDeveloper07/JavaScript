db407c64dda46bc94c8f431413cecc7af592779b
AslDeveloper07 <aslwexking777@gmail.com>
2025-04-27 15:12:21 +0500

    8 problem

--- 1 - uyga vazifa/app.js
+++ 1 - uyga vazifa/app.js
@@ -323,5 +323,14 @@ function fibonacci(n) {
 }
 
 //12 - MASALA
+
+function isPalindrome(str) {
+  let reversed = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return str === reversed;
+}
+
 //13 - MASALA
 //14 - MASALA
