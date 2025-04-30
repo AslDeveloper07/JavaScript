cf6e60ba094aa704c58b3b6efa18338eeea1f342
AslDeveloper07 <aslwexking777@gmail.com>
2025-04-30 21:11:50 +0500

    add

--- 1 - uyga vazifa/app.js
+++ 1 - uyga vazifa/app.js
@@ -633,6 +633,13 @@ if (n > 0) {
 }
 
 //46 - MASALA
+let arr = [3, -2, 5, -1, 0, 7];
+let count = 0;
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > 0) count++;
+}
+console.log("Musbat sonlar soni:", count);
+
 //47- MASALA
 //48 - MASALA
 //49 - MASALA
