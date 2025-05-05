0c686c801df5576329d6e7796492745cf07d68c5
AslDeveloper07 <aslwexking777@gmail.com>
2025-05-05 17:20:24 +0500

    add one problem

--- 1 - uyga vazifa/del.js
+++ 1 - uyga vazifa/del.js
@@ -103,3 +103,22 @@
 // console.log(card.calcTotalAmount());
 
 
+// MASALA
+
+function runLengthEncode(str) {
+    if (str.length === 0) return "";
+
+    let result = "";
+    let count = 1;
+
+    for (let i = 1; i <= str.length; i++) {
+      if (str[i] === str[i - 1]) {
+        count++;
+      } else {
+        result += str[i - 1] + count;
+        count = 1;
+      }
+    }
+
+    return result;
+  }
