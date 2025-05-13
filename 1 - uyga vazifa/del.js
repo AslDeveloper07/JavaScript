923ae1c7dc10a1561248d4c29c97abd001ba37c3
AslDeveloper07 <aslwexking777@gmail.com>
2025-05-13 21:38:39 +0500

    add

--- 1 - uyga vazifa/del.js
+++ 1 - uyga vazifa/del.js
@@ -1,165 +1,166 @@
 // //1 - MASALA
 
-// const card = {
-//   cardProducts: [
-//     {
-//       id: 1,
-//       productName: "olma",
-//       productPrice: 15000,
-//       qualtity: 9
-//     },
-//     {
-//       id: 2,
-//       productName: "olcha",
-//       productPrice: 10200,
-//       qualtity: 5
-//     },
-//     {
-//       id: 3,
-//       productName: "sabzi",
-//       productPrice: 55000,
-//       qualtity: 2
-//     },
-//     {
-//       id: 4,
-//       productName: "kitob",
-//       productPrice: 111000,
-//       qualtity: 3
-//     },
-//     {
-//       id: 5,
-//       productName: "ruchka",
-//       productPrice: 123000,
-//       qualtity: 1
-//     },
-//     {
-//       id: 6,
-//       productName: "kurtka",
-//       productPrice: 15000,
-//       qualtity: 1
-//     },
-//     {
-//       id: 7,
-//       productName: "oyoq kiyim",
-//       productPrice: 17000,
-//       qualtity: 2
-//     },
-//     {
-//       id: 8,
-//       productName: "soat",
-//       productPrice: 15600,
-//       qualtity: 1
-//     }
-//   ],
-//   totalAmount: 0,
-//   totalProduct: 0,
-//   calcTotalAmount() {
-//     this.totalAmount = 0;
-//     for (const item of this.cardProducts) {
-//       this.totalAmount += item.productPrice * item.qualtity;
-//     }
-//     return `Sizning savatchangizda mahsulotlarning umumiy narxi ${this.totalAmount}`;
-//   },
-//   calcTotalProduct() {
-//     this.totalProduct = 0;
-//     for (const item of this.cardProducts) {
-//       this.totalProduct += item.qualtity;
-//     }
-//     return `Sizning savatchangizda mahsulotlarning soni ${this.totalProduct}`;
-//   },
-//   increaseQuantity(id) {
-//     let found = this.cardProducts.find(element => id === element.id);
-//     if (found) {
-//       found.qualtity++;
-//     }
-//     return found ? found.qualtity : "Mahsulot topilmadi";
-//   },
-//   deleteQuantity() {
-//     for (const item of this.cardProducts) {
-//       item.qualtity = 0;
-//     }
-//     this.totalAmount = 0;
-//     this.totalProduct = 0;
-//     return "Barcha mahsulotlarning soni 0 ga tenglandi.";
-//   },
-//   clearQuantityId(id) {
-//     let found = this.cardProducts.find(item => item.id === id);
-//     if (found) {
-//       found.qualtity = 0;
-//       return `ID-si ${id} bo'lgan mahsulotning soni 0 ga tenglandi.`;
-//     } else {
-//       return "Bunday ID-ga ega mahsulot topilmadi.";
-//     }
-//   }
-// };
+const card = {
+  cardProducts: [
+    {
+      id: 1,
+      productName: "olma",
+      productPrice: 15000,
+      qualtity: 9
+    },
+    {
+      id: 2,
+      productName: "olcha",
+      productPrice: 10200,
+      qualtity: 5
+    },
+    {
+      id: 3,
+      productName: "sabzi",
+      productPrice: 55000,
+      qualtity: 2
+    },
+    {
+      id: 4,
+      productName: "kitob",
+      productPrice: 111000,
+      qualtity: 3
+    },
+    {
+      id: 5,
+      productName: "ruchka",
+      productPrice: 123000,
+      qualtity: 1
+    },
+    {
+      id: 6,
+      productName: "kurtka",
+      productPrice: 15000,
+      qualtity: 1
+    },
+    {
+      id: 7,
+      productName: "oyoq kiyim",
+      productPrice: 17000,
+      qualtity: 2
+    },
+    {
+      id: 8,
+      productName: "soat",
+      productPrice: 15600,
+      qualtity: 1
+    }
+  ],
+  totalAmount: 0,
+  totalProduct: 0,
+  calcTotalAmount() {
+    this.totalAmount = 0;
+    for (const item of this.cardProducts) {
+      this.totalAmount += item.productPrice * item.qualtity;
+    }
+    return `Sizning savatchangizda mahsulotlarning umumiy narxi ${this.totalAmount}`;
+  },
+  calcTotalProduct() {
+    this.totalProduct = 0;
+    for (const item of this.cardProducts) {
+      this.totalProduct += item.qualtity;
+    }
+    return `Sizning savatchangizda mahsulotlarning soni ${this.totalProduct}`;
+  },
+  increaseQuantity(id) {
+    let found = this.cardProducts.find(element => id === element.id);
+    if (found) {
+      found.qualtity++;
+    }
+    return found ? found.qualtity : "Mahsulot topilmadi";
+  },
+  deleteQuantity() {
+    for (const item of this.cardProducts) {
+      item.qualtity = 0;
+    }
+    this.totalAmount = 0;
+    this.totalProduct = 0;
+    return "Barcha mahsulotlarning soni 0 ga tenglandi.";
+  },
+  clearQuantityId(id) {
+    let found = this.cardProducts.find(item => item.id === id);
+    if (found) {
+      found.qualtity = 0;
+      return `ID-si ${id} bo'lgan mahsulotning soni 0 ga tenglandi.`;
+    } else {
+      return "Bunday ID-ga ega mahsulot topilmadi.";
+    }
+  }
+};
 
 
-// console.log(card.calcTotalProduct());
+console.log(card.calcTotalProduct());
 
-// console.log(card.clearQuantityId(4));
+console.log(card.clearQuantityId(4));
 
-// console.log(card.calcTotalProduct());
+console.log(card.calcTotalProduct());
 
-// console.log(card.calcTotalAmount());
+console.log(card.calcTotalAmount());
 
 
 // //2-MASALA
 
-// function runLengthEncode(str) {
-//     if (str.length === 0) return "";
+function runLengthEncode(str) {
+    if (str.length === 0) return "";
 
-//     let result = "";
-//     let count = 1;
+    let result = "";
+    let count = 1;
 
-//     for (let i = 1; i <= str.length; i++) {
-//       if (str[i] === str[i - 1]) {
-//         count++;
-//       } else {
-//         result += str[i - 1] + count;
-//         count = 1;
-//       }
-//     }
+    for (let i = 1; i <= str.length; i++) {
+      if (str[i] === str[i - 1]) {
+        count++;
+      } else {
+        result += str[i - 1] + count;
+        count = 1;
+      }
+    }
 
-//     return result;
-//   }
+    return result;
+  }
 
 
 //3-MASALA
 
-// function mostFrequentChar(str) {
-//     const freq = {};
-//     for (let char of str) {
-//       freq[char] = (freq[char] || 0) + 1;
-//     }
+function mostFrequentChar(str) {
+    const freq = {};
+    for (let char of str) {
+      freq[char] = (freq[char] || 0) + 1;
+    }
 
-//     let max = 0;
-//     let result = '';
+    let max = 0;
+    let result = '';
 
-//     for (let char in freq) {
-//       if (freq[char] > max) {
-//         max = freq[char];
-//         result = char;
-//       }
-//     }
+    for (let char in freq) {
+      if (freq[char] > max) {
+        max = freq[char];
+        result = char;
+      }
+    }
 
-//     return result;
-//   }
+    return result;
+  }
 
 
 // //3-MASALA
-// function digitalRoot(n) {
-//     while (n >= 10) {
-//       n = n.toString().split('').reduce((sum, d) => sum + Number(d), 0);
-//     }
-//     return n;
-//   }
 
-//4 - masala
-// function exampleFunction() {
-//   const x = "declared inside function"; // x can only be used in exampleFunction
-//   console.log("Inside function");
-//   console.log(x);
-// }
+function digitalRoot(n) {
+    while (n >= 10) {
+      n = n.toString().split('').reduce((sum, d) => sum + Number(d), 0);
+    }
+    return n;
+  }
+
+4 - masala
+function exampleFunction() {
+  const x = "declared inside function"; // x can only be used in exampleFunction
+  console.log("Inside function");
+  console.log(x);
+}
 
 //5 - masala
 
---
+++ Takrorlash operatori masala/distructure.js
@@ -0,0 +1,89 @@
+const userProfile = {
+  id: 10245,
+  name: {
+    first: "Ali",
+    last: "Karimov",
+    getFullName() {
+      return `${this.first} ${this.last}`;
+    },
+  },
+  contact: {
+    email: "ali.karimov@example.com",
+    phone: "+998901234567",
+    address: {
+      country: "Uzbekistan",
+      city: "Tashkent",
+      street: "Buyuk Ipak Yo'li",
+      zipCode: 100100,
+    },
+  },
+  isActive: true,
+  skills: [
+    { name: "HTML", level: "advanced" },
+    { name: "CSS", level: "advanced" },
+    { name: "JavaScript", level: "intermediate" },
+    { name: "Node.js", level: "beginner" },
+  ],
+  projects: [
+    {
+      title: "Portfolio Website",
+      year: 2024,
+      technologies: ["HTML", "CSS", "JavaScript"],
+      completed: true,
+    },
+    {
+      title: "To-Do App",
+      year: 2025,
+      technologies: ["React", "Tailwind CSS"],
+      completed: false,
+    },
+  ],
+  loginHistory: [
+    { date: "2025-05-01", location: "Tashkent" },
+    { date: "2025-05-10", location: "Samarkand" },
+  ],
+  getSkillNames() {
+    return this.skills.map((skill) => skill.name).join(", ");
+  },
+  addSkill(skillName, level) {
+    this.skills.push({ name: skillName, level });
+  },
+};
+
+// Distructure in JavaScript
+const {
+  id,
+  name: { first, last, getFullName },
+  contact: {
+    email,
+    phone,
+    address: { country, city, street, zipCode },
+  },
+  isActive,
+  skills: [
+    { name: html, level: advanced },
+    { name: css, level: advanceds },
+    { name: javaScript, level: beginner },
+    { name: nodeJs, level: multiLevel },
+  ],
+  projects: [
+    {
+      title: titleName,
+      year: yearOne,
+      technologies: devLanguage,
+      completed: complatedTrue,
+    },
+    {
+      title: titleNames,
+      year: yearTwo,
+      technologies: programLanguage,
+      completed: complatedFalse,
+    },
+  ],
+  loginHistory: [
+    { date:dateNumber, location:loginLocation },
+    { date:dateNumbers, location:loginLocations }
+],
+  getSkillNames,
+  addSkill,
+} = userProfile;
