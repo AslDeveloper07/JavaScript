4c8dc1ab0fa4905d1f583efac66ef7a071595034
AslDeveloper07 <aslwexking777@gmail.com>
2025-05-13 21:41:38 +0500

    add

--- 1 - uyga vazifa/del.js
+++ 1 - uyga vazifa/del.js
@@ -175,3 +175,11 @@ function exampleFunction() {
 
 console.log("Outside function");
 console.log(x);
+
+// 6 masala
+function isPalindrome(str) {
+  str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  return str === str.split('').reverse().join('');
+}
+console.log(isPalindrome("A man, a plan, a canal, Panama")); // true
+
