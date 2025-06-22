6da8e555f8b4796dfa5c6ae67f181e3cecabcb1a
AslDeveloper07 <aslwexking777@gmail.com>
2025-06-22 22:21:12 +0500

    asd

--- OOP/script.js
+++ OOP/script.js
@@ -40,7 +40,6 @@
 //   mercedes.Cars();
 //   mercedes.brake();
 
-
 // class Mashina{
 
 //     constructor(make, speed){
@@ -63,3 +62,75 @@
 // mashina.Mashina()
 // mashina.break()
 
+// class Car {
+//   constructor(make, speed) {
+//     this.make = make;
+//     this.speed = speed;
+//   }
+
+//   accelerate() {
+//     this.speed += 10;
+//     console.log(`${this.make} tezlgii ${this.speed} ga oshdi `);
+//   }
+// }
+
+// const mashina = new Car("Mercedes", 400);
+
+// mashina.accelerate();
+
+
+// Asosiy klass
+class Vehicle {
+    constructor(make, speed) {
+      this.make = make;
+      this.speed = speed;
+    }
+
+    accelerate() {
+      this.speed += 10;
+      console.log(`${this.make} ${this.speed} km/h tezlikka yetdi.`);
+    }
+
+    brake() {
+      this.speed -= 5;
+      console.log(`${this.make} sekinlashib ${this.speed} km/h bo‘ldi.`);
+    }
+  }
+
+  // Vehicle'dan meros olgan Car klassi
+  class Car extends Vehicle {
+    constructor(make, speed, fuel) {
+      super(make, speed); // Vehicle constructor
+      this.fuel = fuel;
+    }
+
+    refuel() {
+      console.log(`${this.make} ga yoqilg‘i quyildi. ${this.fuel} litr mavjud.`);
+    }
+  }
+
+  // Car'dan meros olgan ElectricCar klassi
+  class ElectricCar extends Car {
+    constructor(make, speed, fuel, battery) {
+      super(make, speed, fuel); // Car constructor
+      this.battery = battery;
+    }
+
+    chargeBattery() {
+      console.log(`${this.make} akkumulyatori ${this.battery}% gacha zaryadlandi.`);
+    }
+
+    accelerate() {
+      this.speed += 20;
+      this.battery -= 1;
+      console.log(`${this.make} ${this.speed} km/h bilan ketmoqda. Batareya: ${this.battery}%`);
+    }
+  }
+
+  // Foydalanish
+  const tesla = new ElectricCar("Tesla", 100, 0, 90);
+
+  tesla.accelerate();   // Tesla 120 km/h bilan ketmoqda. Batareya: 89%
+  tesla.brake();        // Tesla sekinlashib 115 km/h bo‘ldi.
+  tesla.chargeBattery();// Tesla akkumulyatori 89% gacha zaryadlandi.
+  tesla.refuel();       // Tesla ga yoqilg‘i quyildi. 0 litr mavjud.
