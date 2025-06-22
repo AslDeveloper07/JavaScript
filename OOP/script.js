791ab9cb94bd9ca49aec500580f3410e0cfd6bcd
AslDeveloper07 <aslwexking777@gmail.com>
2025-06-22 22:02:37 +0500

    da

--- OOP/script.js
+++ OOP/script.js
@@ -1,16 +1,65 @@
-class Car {
-  constructor(make, speed) {
-    this.make = make;
-    this.speed = speed;
-  }
-  accelerate(){
-    this.speed+=10
-    console.log(`${this.make} endi ${this.speed} km/h tezlikka harakatlanmoqda`);
-  }
+// class Car {
+//     constructor(make, speed) {
+//       this.make = make;
+//       this.speed = speed;
+//     }
 
-  brake(){
-    this.speed-=5
-    console.log(`${this.make} tezligi ${this.speed} ga oshdi `);
-    
-  }
-}
+//     accelerate() {
+//       this.speed += 10;
+//       console.log(`${this.make} endi ${this.speed} km/h tezlikka harakatlanmoqda`);
+//     }
+
+//     brake() {
+//       this.speed -= 5;
+//       console.log(`${this.make} tezligi ${this.speed} ga oshdi`);
+//     }
+//   }
+
+//   const bmw = new Car("BMW", 120);
+
+//   bmw.accelerate();
+//   bmw.brake();
+
+// class BMW {
+//     constructor(make, speed) {
+//       this.make = make;
+//       this.speed = speed;
+//     }
+//     Cars() {
+//       this.speed += 10;
+//       console.log(`${this.make} tezilgi ${this.speed} ga oshdi`);
+//     }
+//     brake() {
+//       this.speed -= 10;
+//       console.log(`${this.make} tezligi ${this.speed} ga pasayfi `);
+//     }
+//   }
+
+//   const mercedes = new BMW("Mercedes", 300);
+
+//   mercedes.Cars();
+//   mercedes.brake();
+
+
+// class Mashina{
+
+//     constructor(make, speed){
+//         this.make=make
+//         this.speed=speed
+
+//     }
+//     Mashina(){
+//         this.speed +=20
+//         console.log(`${this.make} tezligi ${this.speed} ga oshdi `);
+//     }
+//     break(){
+//         this.speed-=20
+//         console.log(`${this.make} tezligi ${this.speed} ga kamaydi `);
+
+//     }
+// }
+
+// const mashina = new Mashina("BMW", 800)
+// mashina.Mashina()
+// mashina.break()
+
