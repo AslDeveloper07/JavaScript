0e38d4a11af0cfeaae4357840bf08b56fb4a364d
AslDeveloper07 <aslwexking777@gmail.com>
2025-06-23 15:02:52 +0500

    as

--- API/script.js
+++ API/script.js
@@ -1,28 +1,29 @@
-const url = "https://dummyjson.com/products?limit=10";
+// const url = "https://dummyjson.com/products?limit=10";
 
-const userContainer = document.getElementById("user-container");
+// const userContainer = document.getElementById("user-container");
 
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    userContainer.innerHTML = "";
+// fetch(url)
+//   .then((response) => response.json())
+//   .then((data) => {
+//     userContainer.innerHTML = "";
 
-    data.products.forEach((products) => {
-      const userCard = document.createElement("div");
-      userCard.className =
-        "bg-white rounded-xl shadow-md overflow-hidden hover:shadow-xl transition-all duration-300";
+//     data.products.forEach((products) => {
+//       const userCard = document.createElement("div");
+//       userCard.className =
+//         "bg-white rounded-xl shadow-md overflow-hidden hover:shadow-xl transition-all duration-300";
 
-      const descriptionSentences =
-        products.description.split(".").slice(0, 3).join(".") + ".";
-      userCard.innerHTML = `
-    <img src="${products.images[0]}" alt="${products.title}" class="w-full"/>
-    <div class="p-4">
-    <h2 class="text-gray-800 text-3xl mb-3">${products.title}</h2>
-    <p class="text-lg text-gray-400 ">${descriptionSentences}</p>
-    </div>
+//       const descriptionSentences =
+//         products.description.split(".").slice(0, 3).join(".") + ".";
+//       userCard.innerHTML = `
+//     <img src="${products.images[0]}" alt="${products.title}" class="w-full"/>
+//     <div class="p-4">
+//     <h2 class="text-gray-800 text-3xl mb-3">${products.title}</h2>
+//     <p class="text-lg text-gray-400 ">${descriptionSentences}</p>
+//     </div>
 
-    `;
+//     `;
 
-      userContainer.appendChild(userCard);
-    });
-  });
+//       userContainer.appendChild(userCard);
+//     });
+//   });
+
